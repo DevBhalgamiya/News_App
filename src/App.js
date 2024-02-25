43ab33172cd8ae4dc212000d00784646f7ccb1a3
DevBhalgamiya <devbhalgamiya18@gmail.com>
2024-02-25 22:05:40 +0530

    26/02/24

--- src/App.js
+++ src/App.js
@@ -1,60 +1,100 @@
 
-import './App.css';
+// import './App.css';
 
 
 
-import React, { Component } from 'react'
+// import React, { Component } from 'react';
+// import LoadingBar from 'react-top-loading-bar';
+// import Navbar from './components/Navbar';
+// import News from './components/News';
+
+// import {
+//   BrowserRouter,
+//   Route,
+//   Routes,
+// } from "react-router-dom";
+
+// export default class App extends Component {
+// pageSize=5;
+// apiKey= process.env.REACT_APP_NEWS_API;
+// state={
+//   process: 0,
+// }
+// setProgress =  (process)=>{
+//   this.setState({process:process})
+// }
+//   render() {
+//     return (
+//       <div>
+//       <BrowserRouter>
+//       <Navbar/>
+//       <LoadingBar
+//         color='#f11946'
+//         height={3}
+        
+//         progress={this.state.process}
+      
+//       />
+//       <Routes>
+
+//         <Route exact path="/" element={ <News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize}  country="us" category="general"/>} />
+//         <Route exact path="/business" element={ <News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pageSize={this.pageSize}  country="us" category="business"/>} />
+//         <Route exact path="/entertainment" element={ <News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pageSize={this.pageSize} country="us" category="entertainment"/>} />
+//         <Route exact path="/general" element={ <News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize}  country="us" category="general"/>} />
+//         <Route exact path="/health" element={  <News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize={this.pageSize}  country="us" category="health"/>} />
+//         <Route exact path="/science" element={ <News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize={this.pageSize}  country="us" category="science"/>} />
+//         <Route exact path="/sports" element={ <News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" pageSize={this.pageSize}  country="us" category="sports"/>} />
+//         <Route exact path="/technology" element={ <News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pageSize={this.pageSize}  country="us" category="technology"/>} />
+
+
+
+        
+//       </Routes>
+  
+//     </BrowserRouter>
+//       </div>
+//     )
+//   }
+// }
+
+
+
+
+
+import React, { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import LoadingBar from 'react-top-loading-bar';
 import Navbar from './components/Navbar';
 import News from './components/News';
-import LoadingBar from 'react-top-loading-bar';
 
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  
-} from "react-router-dom";
 
-export default class App extends Component {
-pageSize=5;
-apiKey= process.env.REACT_APP_NEWS_API;
-state={
-  process: 0,
-}
-setProgress =  (process)=>{
-  this.setState({process:process})
-}
-  render() {
-    return (
-      <div>
+export default function App(){
+  const apiKey = process.env.REACT_APP_NEWS_API;
+  const pageSize=5
+  const [selectedCountry, setCountry]=useState('in')
+  return (
+    <div>
       <BrowserRouter>
-      <Navbar/>
-      <LoadingBar
-        color='#f11946'
-        height={3}
+        <Navbar selectedCountry={selectedCountry} onCountryChanged={(country)=>{
+          console.log(country);
+          setCountry(country);
+        }} />
+        {/* <LoadingBar color="#f11946" height={3} progress={30} />
+         */}
+        <News setProgress={()=>{}} apiKey={apiKey} key="Home" pageSize={pageSize} country={selectedCountry} category="Home" />
+        <News setProgress={()=>{}} apiKey={apiKey} key="business" pageSize={pageSize} country={selectedCountry} category="business" />
+        <News setProgress={()=>{}} apiKey={apiKey} key="entertainment" pageSize={pageSize} country={selectedCountry} category="entertainment" />
+        <News setProgress={()=>{}} apiKey={apiKey} key="general" pageSize={pageSize} country={selectedCountry} category="general" />
+        <News setProgress={()=>{}} apiKey={apiKey} key="health" pageSize={pageSize} country={selectedCountry} category="health" />
+        <News setProgress={()=>{}} apiKey={apiKey} key="science" pageSize={pageSize} country={selectedCountry} category="science" />
+        <News setProgress={()=>{}} apiKey={apiKey} key="sports" pageSize={pageSize} country={selectedCountry} category="sports" />
+        <News setProgress={()=>{}} apiKey={apiKey} key="technology" pageSize={pageSize} country={selectedCountry} category="technology" />
         
-        progress={this.state.process}
-      
-      />
-      <Routes>
-
-        <Route exact path="/" element={ <News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize}  country="us" category="general"/>} />
-        <Route exact path="/business" element={ <News setProgress={this.setProgress} apiKey={this.apiKey} mkey="business" pageSize={this.pageSize}  country="us" category="business"/>} />
-        <Route exact path="/entertainment" element={ <News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pageSize={this.pageSize} country="us" category="entertainment"/>} />
-        <Route exact path="/general" element={ <News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize}  country="us" category="general"/>} />
-        <Route exact path="/health" element={  <News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize={this.pageSize}  country="us" category="health"/>} />
-        <Route exact path="/science" element={ <News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize={this.pageSize}  country="us" category="science"/>} />
-        <Route exact path="/sports" element={ <News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" pageSize={this.pageSize}  country="us" category="sports"/>} />
-        <Route exact path="/technology" element={ <News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pageSize={this.pageSize}  country="us" category="technology"/>} />
-
-
-
-        
-      </Routes>
-  
-    </BrowserRouter>
-      </div>
-    )
-  }
+      </BrowserRouter>
+    </div>
+  );
 }
+
+
+
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,62 +1,97 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { Component } from 'react'
-import {
-  Link,
-} from "react-router-dom";
-
-
-
-export class Navbar extends Component {
-  static propTypes = {
-
-  }
-
-  render() {
-    return (
-      <div>
-            <nav className="navbar navbar-expand-lg bg-body-dark">
-  <div className="container-fluid">
-    <Link className="navbar-brand" to="#">Vasu Dholakiya</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link " aria-current="page" to="/">Home</Link>
-        </li>
-      
-         <li className="nav-item">
-          <Link className="nav-link" to="/business">Business </Link>
-        </li>
-         <li className="nav-item">
-          <Link className="nav-link" to="/entertainment">Entertainment</Link>
-        </li>
-         <li className="nav-item">
-          <Link className="nav-link" to="/general">General </Link>
-        </li>
-         <li className="nav-item">
-          <Link className="nav-link" to="/health">Health</Link>
-        </li>
-         <li className="nav-item">
-          <Link className="nav-link" to="/science">Science </Link>
-        </li>
-         <li className="nav-item">
-          <Link className="nav-link" to="/sports">Sports</Link>
-        </li>
-         <li className="nav-item">
-          <Link className="nav-link" to="/technology">Technology</Link>
-        </li>
-        
-        
-      </ul>
-     
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'
+export default function Navbar({ onCountryChanged,selectedCountry }) {
+  const [toggle, setToggle] = useState(false)
+  
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Byte Bulletins
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/business">
+                  Business
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/entertainment">
+                  Entertainment
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/general">
+                  General
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/health">
+                  Health
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/science">
+                  Science
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/sports">
+                  Sports
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/technology">
+                  Technology
+                </Link>
+              </li>
+            </ul>
+            <button onClick={() => {
+              setToggle(!toggle)
+            }} className="btn">
+              Choose Country
+            </button>
+            {toggle && (
+              <ul className="list-group" style={{ cursor: 'pointer', marginRight: '10px' }}>
+                <li className={`list-group-item ${selectedCountry === 'us' && 'active'}`} onClick={() => onCountryChanged('us')}>
+                  USA
+                </li>
+                <li className={`list-group-item ${selectedCountry === 'in' && 'active'}`} onClick={() => onCountryChanged('in')}>
+                  India
+                </li>
+                <li className={`list-group-item ${selectedCountry === 'jp' && 'active'}`} onClick={() => onCountryChanged('jp')}>
+                  Japan
+                </li>
+                <li className={`list-group-item ${selectedCountry === 'ua' && 'active'}`} onClick={() => onCountryChanged('ua')}>
+                  UAE
+                </li>
+                <li className={`list-group-item ${selectedCountry === 'ru' && 'active'}`} onClick={() => onCountryChanged('ru')}>
+                  Russia
+                </li>
+              </ul>
+            )}
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
-      </div>
-    )
-  }
+  )
 }
-
-export default Navbar
